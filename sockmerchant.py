2f86711ca1b9fca592c7508ebc680627ff5dbc69
Poornapragna <poornapragna@gmail.com>
2019-09-05 10:34:17 -0400

    Adding some comments.

--- sockmerchant.py
+++ sockmerchant.py
@@ -9,14 +9,18 @@ import sys
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar):
     result = 0
+    # Create a list of sock color and whether it is paired or not.
     paired_list = [[sock,0] for sock in ar]
     for sock_pair,i in zip(paired_list,range(n)):
+        # if paired don't compare again
         if sock_pair[1]:
             continue
         for next_sock,j in zip(paired_list[i+1:],range(i+1,n)):
+            # if paired don't compare again
             if next_sock[1]:
                 continue
             if sock_pair[0] == next_sock[0]:
+                # Pair found. Make pair flag true for both socks.
                 paired_list[j][1] = True
                 paired_list[i][1] = True
                 result = result+1
