4e4162f76170a72cb736708a0ae7c57df2f94061
Poornapragna <poornapragna@gmail.com>
2019-09-05 10:32:48 -0400

    sock merchant challenge

---
+++ sockmerchant.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    result = 0
+    paired_list = [[sock,0] for sock in ar]
+    for sock_pair,i in zip(paired_list,range(n)):
+        if sock_pair[1]:
+            continue
+        for next_sock,j in zip(paired_list[i+1:],range(i+1,n)):
+            if next_sock[1]:
+                continue
+            if sock_pair[0] == next_sock[0]:
+                paired_list[j][1] = True
+                paired_list[i][1] = True
+                result = result+1
+                break
+    return result
+
+
+
+if __name__ == '__main__':
+    fptr = open('output.txt', 'w')
+
+    #n = int(input('Enter number of socks - '))
+
+    ar = list(map(int, input('Enter sock colors as integers seperated by space - ').rstrip().split()))
+
+    n = len(ar)
+
+    result = sockMerchant(n, ar)
+
+    print('Number of pairs - '+str(result))
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
